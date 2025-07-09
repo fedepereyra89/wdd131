6cfda572c249062fee8fc27c35c44ec7772c8179
fedepereyra89 <fedepereyra89@gmail.com>
2025-07-09 00:33:29 -0300

    all good

---
+++ scripts/temples.js
@@ -0,0 +1,11 @@
+// select the DOM elements for output
+const currentYear = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastmodified");
+
+
+const today = new Date();
+const dateLastModified = new Date(document.lastModified);
+
+
+currentYear.textContent = today.getFullYear();
+lastModified.textContent = `Last modification: ${dateLastModified.toLocaleString()}`;
